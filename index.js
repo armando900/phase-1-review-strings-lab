b6f33b146858032f2e6ac63bc30daabb2c23f0fb
Armando <seniorbuso@gmail.com>
2021-12-13 14:44:52 -0800

    commit tree 3348bf4087741e68175ad70737fb29863d4d5aa7

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'name';
+
+const welcomeMessage = 'Welcome to Flatbook, ${name} !';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${name.slice(0)}!`;
+
